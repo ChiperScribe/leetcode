4510ea3506d69eab4c8d057296b0840e1ade22fd
이재필 <harry_jp0830@naver.com>
2024-01-13 15:48:43 +0900

    initialize

---
+++ 2.4.py
@@ -0,0 +1,14 @@
+import collections
+import re
+from typing import List
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned = List[str]) -> str:
+
+        word_list = re.sub(r'^\w', ' ', paragraph).lower().split()
+        filtered_words = [word for word in word_list if word not in banned]
+
+        counts = collections.defaultdict(int)
+        for word in filtered_words:
+            counts[word] += 1
+
+        return max(counts, key=counts.get)
