e992469b57ce8299f538d3f52691fdf200625046
이재필 <harry_jp0830@naver.com>
2024-01-13 15:51:06 +0900

    modify

--- 2.4.py
+++ 2.4.py
@@ -4,7 +4,7 @@ from typing import List
 class Solution:
     def mostCommonWord(self, paragraph: str, banned = List[str]) -> str:
 
-        word_list = re.sub(r'^\w', ' ', paragraph).lower().split()
+        word_list = re.sub(r'[^\w]', ' ', paragraph).lower().split()
         filtered_words = [word for word in word_list if word not in banned]
 
         counts = collections.defaultdict(int)
