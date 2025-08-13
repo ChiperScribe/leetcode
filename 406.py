7cacce9155551c6785fd4644cd2d8e415598549f
Lee JaePil <75873213+ChiperScribe@users.noreply.github.com>
2025-08-13 14:59:40 +0900

    Update 406.py

--- 406.py
+++ 406.py
@@ -1,3 +1,4 @@
+## 1
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         # x[0]: 키, x[1]: 키 큰 사람의 수
@@ -6,4 +7,18 @@ class Solution:
         
         for p in people:
             res.insert(p[1], p)
+        return res
+
+## 2
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        # x[0]: 키, x[1]: 키 큰 사람의 수
+        heap = []
+        for p in people:
+            heapq.heappush(heap, (-p[0], p[1]))
+
+        res = []
+        while heap:
+            person = heapq.heappop(heap)
+            res.insert(person[1], [-person[0], person[1]])
         return res
