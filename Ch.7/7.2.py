a4c6c698f34177331cdc819d15ab0d5e53d9005d
Lee JaePil <75873213+ChiperScribe@users.noreply.github.com>
2025-08-13 14:40:49 +0900

    Delete Ch.7 directory

--- Ch.7/7.2.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Solve by using 'in'
-        for i, n in enumerate(nums):
-            complement = target - n
-
-            if complement in nums[i + 1:]:
-                return [i, nums[i + 1:].index(complement) + (i + 1)]
